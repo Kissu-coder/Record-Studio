f492e9174996197ebf00b8a2ac511146d346c71f
Kissu <69744388+Kissu-coder@users.noreply.github.com>
2021-02-14 14:29:43 +0530

    fixed bug

--- src/render.js
+++ src/render.js
@@ -1,12 +1,28 @@
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+
 const videoElement = document.querySelector('video');
 const startBtn = document.querySelector('startBtn');
 const stopBtn = document.querySelector('stopBtn');
 const videoSelectBtn = document.getElementById('videoSelection');
+console.log(videoSelectBtn)
 
-const { desktopCapturer } = require('electron');
+videoSelectBtn.onclick = getVideoSources();
 
 async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+    });
 
-    
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource()
+            };
+        })
+    );
 
+    videoOptionsMenu.popup();
+    console.log(videoOptionsMenu)
 }
