0871783dd6f893f4483555523c040f03164a394d
Kissu <69744388+Kissu-coder@users.noreply.github.com>
2021-02-14 14:03:13 +0530

    first commit

---
+++ src/render.js
@@ -0,0 +1,12 @@
+const videoElement = document.querySelector('video');
+const startBtn = document.querySelector('startBtn');
+const stopBtn = document.querySelector('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelection');
+
+const { desktopCapturer } = require('electron');
+
+async function getVideoSources() {
+
+    
+
+}
