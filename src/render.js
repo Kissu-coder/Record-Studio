d26b268bcaf07e6a958ef5b52bf884d7ac36ddc2
Kissu <69744388+Kissu-coder@users.noreply.github.com>
2021-02-14 14:48:42 +0530

    almost done!

--- src/render.js
+++ src/render.js
@@ -1,13 +1,13 @@
 const { desktopCapturer, remote } = require('electron');
-const { Menu } = remote;
+const { dialog, Menu } = remote;
+const { writeFile } = require('fs');
 
 const videoElement = document.querySelector('video');
 const startBtn = document.querySelector('startBtn');
 const stopBtn = document.querySelector('stopBtn');
-const videoSelectBtn = document.getElementById('videoSelection');
-console.log(videoSelectBtn)
 
-videoSelectBtn.onclick = getVideoSources();
+const videoSelectBtn = document.getElementById('videoSelection');
+videoSelectBtn.onclick = getVideoSources;
 
 async function getVideoSources() {
     const inputSources = await desktopCapturer.getSources({
@@ -18,11 +18,63 @@ async function getVideoSources() {
         inputSources.map(source => {
             return {
                 label: source.name,
-                click: () => selectSource()
+                click: () => selectSource(source)
             };
         })
     );
 
     videoOptionsMenu.popup();
-    console.log(videoOptionsMenu)
+}
+
+let mediaRecoder;
+const recordedChunks = [];
+
+async function selectSource(source) {
+    videoSelectBtn.innerText = source.name;
+
+    const constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+    };
+    //create a stream
+    const stream = await navigator.mediaDevices
+        .getUserMedia(constraints);
+
+    //preview the source in a video element
+    videoElement.srcObject = stream;
+    videoElement.play();
+
+    // create the media recoder
+    const options = { mimeType: 'video/webm, codecs=vp9' };
+    mediaRecoder = new mediaRecoder(stream, options);
+
+    // register event handlers
+    mediaRecoder.ondataavailable = handleDataAvailable;
+    mediaRecoder.options = handleStop;
+
+    // captures all recorded chunks
+    function handleDataAvailable(e) {
+        console.log('Video data available!');
+        recordedChunks.push(e.data);
+    }
+
+    async function handleStop(e) {
+        const blob = new Blob(recordedChunks, {
+            type: 'video/webm; codecs=vp9'
+        });
+
+        const buffer = Buffer.from(await blob.arrayBuffer());
+        //raw data done!!!!!
+
+
+        const { filePath } = await dialog.showSaveDialog({
+            buttonLabel: 'Save Video!',
+            defaultPath: 'vid-${Date.now()}.webm'
+        });
+    }
 }
